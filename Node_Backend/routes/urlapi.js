92b45dc8ffc5e85af4c1e5a077d4d5df72b7da1b
sorawich tangsriphairoj <zerostriketeam@gmail.com>
2021-09-30 23:53:01 +0700

    Initial NodeJS_BackEnd

---
+++ Node_Backend/routes/urlapi.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+var generator = require('generate-password');
+var mqtt = require('mqtt')
+
+const option = {
+  username: "cpre_softdev",
+  password: "2iyi7xdCR3rLjX6b4V6e99oKHwnAwFcQ"
+};
+
+router.all('/', function(req, res, next) {
+
+    let {name} = req.body
+
+    var key = generator.generate({length: 10,numbers: true});
+    const data = {"datatype": 1, "Table": name,"token": key}
+    const jsonStr = JSON.stringify(data);
+    
+    let web  = `https://www.ordishes.com/?token=${key}`;
+    
+
+    const client = mqtt.connect("mqtt://192.168.42.201:1884",option);
+    
+    client.on('connect', function () {
+          client.publish('/urlapi/', jsonStr)
+          console.log("Connected OK")
+        })
+    
+    client.on('message', function (topic, message) {
+      console.log(message.toString())
+      client.end()
+    })
+    console.log(jsonStr);
+    res.send(web);
+  });
+  
+module.exports = router;
+
