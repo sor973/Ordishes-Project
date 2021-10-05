153d1f142b5425159087a9a7e3af9fceee88cbfd
Sitthikorn Aimkhon <basza5589@gmail.com>
2021-10-06 02:00:44 +0700

    Add date and time to Appcheckbill and add shadow and icon

--- React_Frontend/src/Components/AppCheckbill.js
+++ React_Frontend/src/Components/AppCheckbill.js
@@ -1,69 +1,90 @@
+
 import React from 'react'
-import { Container, Row, Col, Table } from 'react-bootstrap';
+import moment from 'moment';
+import { Container, Row, Col, Table, Card } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUtensils } from '@fortawesome/free-solid-svg-icons'
 
 function AppCheckbill() {
-    
-    var today = new Date(),
-    dateToday = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-    console.log(dateToday);
     return (
         <Container>
-            <Row>
-                <Col className="d-flex justify-content-center"><h3>Ordishes</h3></Col>
-            </Row>
-            <Row>
-                <Col className="d-flex justify-content-center"><p>1518 Pracharat 1 Road,Wongsawang,<br/> Bangsue, Bangkok 10800 Thailand.</p></Col>
-            </Row>
-            <Row>
-                <Col className="d-flex justify-content-center"><p>Tel:000-000-0000</p></Col>
-            </Row>
-            <Row>
-                <Col className="d-flex justify-content-center"><p>Email:ordishes@test.com</p></Col>
-            </Row>
-            <Row>
-                <Col className="d-flex justify-content-center">
-                    <p>Date: {dateToday}</p>
-                </Col>
-                <Col className="d-flex justify-content-center">
-                    <p>Check:</p>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <Table responsive="sm" size="sm" >
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Qty</th>
-                                <th>Price</th>
-                                <th>Total</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>steak t-bone</td>
-                                <td>2</td>
-                                <td>10</td>
-                                <td>20</td>
-                            </tr>
-                            <tr>
-                                <td>steak t-bone</td>
-                                <td>2</td>
-                                <td>10</td>
-                                <td>20</td>
-                            </tr>
-                            <tr>
-                                <td>steak t-bone</td>
-                                <td>2</td>
-                                <td>10</td>
-                                <td>20</td>
-                            </tr>
-                        </tbody>
-                    </Table>
-                </Col>
-            </Row>
-            
-
+            <Card className="mt-3 shadow p-3 mb-5 bg-white rounded">
+                <Row className="mt-3">
+                    <Col className="d-flex justify-content-center"><h3>Ordishes <FontAwesomeIcon icon={faUtensils} /></h3></Col>
+                </Row>
+                <Row>
+                    <Col className="d-flex justify-content-center"><p>1518 Pracharat 1 Road,Wongsawang,<br /> Bangsue, Bangkok 10800 Thailand.</p></Col>
+                </Row>
+                <Row>
+                    <Col className="d-flex justify-content-center"><p>Tel: 000-000-0000</p></Col>
+                </Row>
+                <Row>
+                    <Col className="d-flex justify-content-center"><p>Email: ordishes@test.com</p></Col>
+                </Row>
+                <Row>
+                    <Col className="d-flex justify-content-center">
+                        <p>Date: {moment().format('DD/MM/YYYY')}</p>
+                    </Col>
+                    <Col className="d-flex justify-content-center">
+                        <p>Time: {moment().format('HH:mm')}</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Table striped responsive="sm" size="sm" >
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Qty</th>
+                                    <th>Price</th>
+                                    <th>Total</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>steak t-bone</td>
+                                    <td>2</td>
+                                    <td>10</td>
+                                    <td>20</td>
+                                </tr>
+                                <tr>
+                                    <td>steak t-bone</td>
+                                    <td>2</td>
+                                    <td>10</td>
+                                    <td>20</td>
+                                </tr>
+                                <tr>
+                                    <td>steak t-bone</td>
+                                    <td>2</td>
+                                    <td>10</td>
+                                    <td>20</td>
+                                </tr>
+                                <tr>
+                                    <td>steak t-bone</td>
+                                    <td>2</td>
+                                    <td>10</td>
+                                    <td>20</td>
+                                </tr>
+                                <tr>
+                                    <td colSpan="3"><strong>Total Price :</strong></td>
+                                    <td><strong>60</strong></td>
+                                </tr>
+                                <tr>
+                                    <td colSpan="3">Tax 7% :</td>
+                                    <td>4.2</td>
+                                </tr>
+                                <tr>
+                                    <td colSpan="3"><strong>Pay amount :</strong></td>
+                                    <td><strong>64.2</strong></td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="d-flex justify-content-center"><h6>Thank You </h6></Col>
+                </Row>
+            </Card>
         </Container>
     )
 }
