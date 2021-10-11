126022beda6fecfb2d2ca58e9bc6db71c232dbad
Sitthikorn Aimkhon <basza5589@gmail.com>
2021-10-11 23:06:06 +0700

    Modify layout add icon and shadow AppQRCodeGen

--- React_Frontend/src/Components/AppQRCodeGen.js
+++ React_Frontend/src/Components/AppQRCodeGen.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { Container, Row, Col, Form, Button, Image, Alert } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button, Image, Alert, Card } from 'react-bootstrap';
 import axios from 'axios';
 import QRcode from 'qrcode';
 import { axiosConfiguration } from '../variable/axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQrcode } from '@fortawesome/free-solid-svg-icons'
 
 function AppQRCodeGen() {
 
@@ -44,29 +46,34 @@ function AppQRCodeGen() {
     console.log(qrcode);
 
     return (
-        <Container className="mt-3">
-            <Row>
-                <Col>
-                    <Form onSubmit={postTable}>
-                        <Form.Group>
-                            <Form.Label>Enter table</Form.Label>
-                            <Form.Control type="number" placeholder="Enter table" value={name} onChange={(e) => setName(e.target.value)} />
-                            <Alert variant="danger" show={show} className="mt-3">{ errors && <div>{errors}</div> }</Alert>
-                        </Form.Group>
-                        <Button variant="primary" type="submit" className="d-flex justify-content-center">Submit</Button>
-                    </Form>
-                </Col>
-            </Row>
-            <Row>
-                <Col className="d-flex justify-content-center mt-3 mb-3">
-                    <Button onClick={qr} variant="outline-secondary">Generate QR Code</Button>
-                </Col>
-            </Row>
-            <Row>
-                <Col className="d-flex justify-content-center">
-                    <Image src={qrcode} width={200} height={200} />
-                </Col>
-            </Row>
+        <Container>
+            <Card className="mt-3 shadow p-3 mb-5 bg-white rounded">
+                <Card.Body>
+                    <Card.Title>QR Code generator </Card.Title>
+                    <Row>
+                        <Col>
+                            <Form onSubmit={postTable}>
+                                <Form.Group>
+                                    <Form.Label>Enter table</Form.Label>
+                                    <Form.Control type="number" placeholder="Enter table" value={name} onChange={(e) => setName(e.target.value)} />
+                                    <Alert variant="danger" show={show} className="mt-3">{errors && <div>{errors}</div>}</Alert>
+                                </Form.Group>
+                                <Button variant="primary" type="submit" className="d-flex justify-content-center">Submit</Button>
+                            </Form>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="d-flex justify-content-center mt-3 mb-3">
+                            <Button onClick={qr} variant="outline-secondary">Generate QR Code <FontAwesomeIcon icon={faQrcode} /></Button>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="d-flex justify-content-center">
+                            <Image src={qrcode} width={200} height={200} />
+                        </Col>
+                    </Row>
+                </Card.Body>
+            </Card>
         </Container>
     )
 }
