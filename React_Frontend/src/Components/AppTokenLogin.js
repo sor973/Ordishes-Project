c2281da7c1507603ee8e46b7c915926707024671
Sitthikorn Aimkhon <basza5589@gmail.com>
2021-10-14 15:43:00 +0700

    Redesign AppTokenLogin

--- React_Frontend/src/Components/AppTokenLogin.js
+++ React_Frontend/src/Components/AppTokenLogin.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Row, Col, Form, Button, Alert } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button, Alert, Card } from 'react-bootstrap';
 import { Redirect, useLocation } from 'react-router-dom'
 import QueryString from 'query-string';
 
@@ -51,25 +51,29 @@ function AppTokenLogin() {
 
     return (
         <Container className="mt-3">
-            <h3 className="text-center">Token Login</h3>
-            <Row>
-                <Col className="d-flex justify-content-center">
-                    <Form>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Enter Token</Form.Label>
-                            <Form.Control type="text" placeholder="Enter Token" value={token}  onChange={changeHandling}/>
-                            <Form.Text className="text-muted">
-                                Token will expire in 24 hr.
-                            </Form.Text>
-                        </Form.Group>
-                        <Button variant="primary" type="submit" disabled={errorstatus} onClick={submitHandling}>Submit</Button>
-                    </Form>
+            <Row className="">
+                <Col className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                    <Card className="my-5 bg-dark text-light shadow" style={{"border-radius":"1rem"}}>
+                        <Card.Body>
+                            <Card.Title><h3 className="text-center">Token Login</h3></Card.Title>
+                            <Form>
+                                <Form.Group className="mb-3" controlId="formBasicEmail">
+                                    <Form.Label>Enter Token</Form.Label>
+                                    <Form.Control type="text" placeholder="Enter Token" value={token} onChange={changeHandling} />
+                                    <Form.Text className="text-muted">
+                                        Token will expire in 24 hr.
+                                    </Form.Text>
+                                </Form.Group>
+                                <Button variant="primary" type="submit" disabled={errorstatus} onClick={submitHandling} className="w-100">Login via token</Button>
+                            </Form>
+                            {showerror && <Container className="d-flex justify-content-center align-items-center mt-3">
+                                <Alert variant="danger" className="text-center">{errortext}</Alert>
+                            </Container>}
+                            {redirect}
+                        </Card.Body>
+                    </Card>
                 </Col>
             </Row>
-            {showerror && <Container className="d-flex justify-content-center align-items-center my-3">
-                <Alert variant="danger" className="text-center">{errortext}</Alert>
-            </Container>}
-            {redirect}
         </Container>
     )
 }
