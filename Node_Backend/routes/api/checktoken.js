dae03763b95c8bb785b55307dba869412a8f3bc0
Punyapat Meema <thew2001@hotmail.com>
2021-10-14 22:19:03 +0700

    Edit and redesign Token check API

--- Node_Backend/routes/api/checktoken.js
+++ Node_Backend/routes/api/checktoken.js
@@ -7,26 +7,26 @@ const url = 'mongodb://cpre_softdev:xh8Av6Qqe6goj66Ms7gr9nxiv4N6J4ZZ@192.168.42.
 const dbName = 'development';
 
 
-router.get('/', async function (req, res, next) {
+router.all('/', async function (req, res, next) {
 
     // const menulist = [];
     const client = new MongoClient(url);
     await client.connect();
     const db = client.db(dbName);
-    const collection = db.collection('development_stage');
-    const findmenu = await collection.find({ "token": res.token }).toArray();
-
+    const collection = db.collection('token');
+    const findResult = await collection.find({ "token": req.body.token }).toArray();
+    console.log(findResult);
     if (_.isEmpty(findResult)) {
-        return res.status(400).send({
+        return res.status(200).send({
             "exist": false,
             "message": "Token not avaliable"
-          });
+        });
     }
     if (!_.isEmpty(findResult)) {
         return res.send({
             "exist": true,
             "message": "Token avaliable"
-          });
+        });
     }
 
 });
