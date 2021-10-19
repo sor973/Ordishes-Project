cebb9aaa26200c04e07dbc15106fa0bb64d29bba
Punyapat Meema <thew2001@hotmail.com>
2021-10-19 21:22:18 +0700

    Implement Token Check System for status page

--- React_Frontend/src/Components/AppStatus.js
+++ React_Frontend/src/Components/AppStatus.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col, Card, Table } from 'react-bootstrap';
+import { useState, useEffect, useRef } from 'react';
+import { Redirect } from 'react-router-dom'
+import { TokenAuth } from '../functions/tokenAuth';
 
 function AppStatus() {
+
+    const [redirect, setredirect] = useState();
+    const componentIsMounted = useRef(true);
+
+    useEffect(()=>{
+        async function doAuth(){
+            if(!localStorage.getItem('token')) if(componentIsMounted.current) return setredirect(<Redirect to="/" />);
+            let getTokenStatus = await TokenAuth.tokenAuthCheck(localStorage.getItem('token'))
+            if(!getTokenStatus){
+                if(componentIsMounted.current) return setredirect(<Redirect to="/" />);
+            }
+        } 
+        doAuth();
+        return () => {
+            componentIsMounted.current = false;
+        };
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
     return (
         <Container>
+            {redirect}
             <Card className="mt-3 shadow p-3 mb-5 bg-white rounded">
                 <Card.Title>Status</Card.Title>
                 <Row>
