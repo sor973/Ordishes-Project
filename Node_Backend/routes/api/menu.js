2940b3fae58eeea1b5c9425e97ca658c30b817a0
Punyapat Meema <thew2001@hotmail.com>
2021-10-26 22:38:11 +0700

    Implement Single DB connection to all of the api

--- Node_Backend/routes/api/menu.js
+++ Node_Backend/routes/api/menu.js
@@ -1,17 +1,13 @@
 var express = require('express');
 var router = express.Router();
-const { MongoClient } = require('mongodb');
-
-const url = 'mongodb://cpre_softdev:xh8Av6Qqe6goj66Ms7gr9nxiv4N6J4ZZ@192.168.42.201:27017/?authSource=cpreauth&ssl=false';
-const dbName = 'ordishes';
+const Mongodb = require('../../database/MongoClient');
 
 
 router.get('/', async function(req, res, next) {
-  
+    await Mongodb.function.mongodbInitAndConnect();
+    const client = Mongodb.instance.client;
     // const menulist = [];
-    const client = new MongoClient(url);
-    await client.connect();
-    const db = client.db(dbName);
+    const db = client.db(Mongodb.variable.database_name);
     const collection = db.collection('Menu');
     const findmenu = await collection.find({datatype:20}).toArray();
     // menulist.push(findmenu)
--- Node_Backend/routes/api/order.js
+++ Node_Backend/routes/api/order.js
@@ -3,11 +3,8 @@ var router = express.Router();
 var mqtt = require('mqtt')
 var _ = require('lodash');
 const { v4: uuidv4 } = require('uuid');
-const { MongoClient } = require('mongodb');
 const { range } = require('lodash');
-
-const url = 'mongodb://cpre_softdev:xh8Av6Qqe6goj66Ms7gr9nxiv4N6J4ZZ@192.168.42.201:27017/?authSource=cpreauth&ssl=false';
-const dbName = 'ordishes';
+const Mongodb = require('../../database/MongoClient');
 
 const option = {
   username: "cpre_softdev",
@@ -15,10 +12,9 @@ const option = {
 };
 
 router.all('/', async function(req, res, next) {
-
-    const client = new MongoClient(url);
-    await client.connect();
-    const db = client.db(dbName);
+    await Mongodb.function.mongodbInitAndConnect();
+    const client = Mongodb.instance.client;
+    const db = client.db(Mongodb.variable.database_name);
     const collection = db.collection('Order_for_Kitchen');
 
     var resp = req.body;
